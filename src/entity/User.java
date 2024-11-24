77d5c7e70902182b5bbfc9e4d1e9ffc3e3ed94f8
Zhenpeng Lai <zhenpeng_lai@163.com>
2024-11-24 19:59:03 +0800

    初始化 back-end 分支，添加基础代码

---
+++ src/entity/User.java
@@ -0,0 +1,82 @@
+package com.farmland.intel.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author
+ * @since 2022-01-26
+ */
+@Getter
+@Setter
+@TableName("sys_user")
+@ApiModel(value = "User对象", description = "")
+@ToString
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty("用户名")
+    private String username;
+
+    @ApiModelProperty("密码")
+    private String password;
+
+    @ApiModelProperty("昵称")
+    private String nickname;
+
+    @ApiModelProperty("邮箱")
+    private String email;
+
+    @ApiModelProperty("电话")
+    private String phone;
+
+    private String social;
+
+    @ApiModelProperty("地址")
+    private String address;
+
+    @ApiModelProperty("创建时间")
+    private Date createTime;
+
+    @ApiModelProperty("头像")
+    private String avatarUrl;
+
+    @ApiModelProperty("角色")
+    private String role;
+
+    private Boolean collectFlag;
+
+    @TableField(exist = false)
+    List<String> phoneList;
+
+    @TableField(exist = false)
+    List<String> emailList;
+
+    @TableField(exist = false)
+    List<String> socialList;
+
+    @TableField(exist = false)
+    List<String> addressList;
+
+}
---
+++ src/mapper/NoticeMapper.java
@@ -0,0 +1,16 @@
+package com.farmland.intel.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.farmland.intel.entity.Notice;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2023-03-14
+ */
+public interface NoticeMapper extends BaseMapper<Notice> {
+
+}
